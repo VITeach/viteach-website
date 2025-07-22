894218606409784e659b7fcdcede33da1b9a0f7c
adityapandey23 <adityapandey2395@gmail.com>
2025-07-22 07:51:56 +0530

    added motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import * as motion from "motion/react-client";
 import React from 'react'
 
 const Root = () => {
   return (
-    <div>Root
-      <div className='m-10'>
-        <Button>Click me</Button>
-      </div>
+    <div>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className='m-10 flex flex-col items-center justify-center gap-y-2'>
+          Can you click on this button ?
+          <Button onClick={() => {
+            alert("You clicked the button")
+          }}>Click me</Button>
+        </div>
+      </motion.div>
     </div>
   )
 }
