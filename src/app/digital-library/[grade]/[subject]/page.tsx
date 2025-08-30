e82843872a7cf3ad973e84dd0c089a14a4f03e86
adityapandey23 <adityapandey2395@gmail.com>
2025-08-30 14:41:13 +0530

    it worked, peace

--- src/app/digital-library/[grade]/[subject]/page.tsx
+++ src/app/digital-library/[grade]/[subject]/page.tsx
@@ -1,16 +1,19 @@
 // If you are seeing this, then the issue probably got fixed and I never cared about removing this comment
 // Just so you know, I was debugging this in production (because who the fk needs tests)
 // Also, nextjs is so shitty that it doesn't let me name a dynamic route to be class because it might break something
+// Also, this is a really fucking dumb way to do this, but I was too lazy to fix it
+// Also, I'm not even sure why it works, but it does
+
 import { notFound } from 'next/navigation';
 
 const subjects = ['science', 'maths', 'english'];
 
-export default async function DigitalLibraryContent({
+const DigitalLibraryContent = async ({
   params,
 }: {
-  params: Promise<{ grade: string; subject: string }>;
-}) {
-  const { grade, subject } = await params;
+  params: Promise<{ grade: string; subject: string }>; // The promise is needed because the params are not available in the page component :)
+}) => {
+  const { grade, subject } = await params; // Don't trust the linter, it is needed
   const classNumber = parseInt(grade, 10);
 
   if (isNaN(classNumber) || classNumber < 1 || classNumber > 12) {
@@ -30,4 +33,6 @@ export default async function DigitalLibraryContent({
       </div>
     </div>
   );
-}
+};
+
+export default DigitalLibraryContent;
