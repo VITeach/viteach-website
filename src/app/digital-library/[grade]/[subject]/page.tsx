0588312a2e4636e41d51765aa81fedf2dd27a084
adityapandey23 <adityapandey2395@gmail.com>
2025-08-30 14:30:49 +0530

    i want to jump

--- src/app/digital-library/[grade]/[subject]/page.tsx
+++ src/app/digital-library/[grade]/[subject]/page.tsx
@@ -4,26 +4,23 @@ const subjects = ['science', 'maths', 'english'];
 
 interface PageProps {
   params: {
-    class: string;
+    grade: string;
     subject: string;
   };
 }
 
 const DigitalLibraryContent = ({ params }: PageProps) => {
-  const { class: classParam, subject: subjectParam } = params;
   // If you are seeing this, then the issue probably got fixed and I never cared about removing this comment
   // Just so you know, I was debugging this in production (because who the fk needs tests)
   // Also, nextjs is so shitty that it doesn't let me name a dynamic route to be class because it might break something
+  const { grade, subject } = params;
+  const classNumber = parseInt(grade, 10);
 
-  const classNumber = parseInt(classParam);
-
-  // Validate class number
-  if (classNumber < 1 || classNumber > 12) {
+  if (isNaN(classNumber) || classNumber < 1 || classNumber > 12) {
     return notFound();
   }
 
-  // Validate subject
-  if (!subjectParam || !subjects.includes(subjectParam)) {
+  if (!subjects.includes(subject)) {
     return notFound();
   }
 
@@ -31,7 +28,7 @@ const DigitalLibraryContent = ({ params }: PageProps) => {
     <div className="flex flex-col items-center justify-center h-screen gap-y-4">
       <h1>Digital Library Content</h1>
       <div className="flex flex-col items-center justify-center gap-y-4 text-2xl font-bold text-red-900">
-        <h2>Subject: {subjectParam}</h2>
+        <h2>Subject: {subject}</h2>
         <h2>Class: {classNumber}</h2>
       </div>
     </div>
