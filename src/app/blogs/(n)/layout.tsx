d7def19580ee4d8d6824651e4f8197d68b1a44b5
adityapandey23 <adityapandey2395@gmail.com>
2025-09-03 14:50:32 +0530

    content updated in blogs

--- src/app/blogs/(n)/layout.tsx
+++ src/app/blogs/(n)/layout.tsx
@@ -5,11 +5,9 @@ import React from 'react';
 const BlogLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <Navbar />
       <article className="max-w-4xl mx-auto px-4 py-8 my-10">
         <div className="max-w-none">{children}</div>
       </article>
-      <Footer />
     </div>
   );
 };
